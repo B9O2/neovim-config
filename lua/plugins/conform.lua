40a1aac3677926eaba4c656fc98944bc87ea6f7b
Felix <112383606+B9O2@users.noreply.github.com>
2025-10-11 19:05:10 +0800

    typescript

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,6 +5,10 @@ return {
       -- 核心配置就在这里
       -- 添加 rustfmt 作为 Rust 文件的格式化程序
       rust = { "rustfmt" },
+      javascript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescript = { "prettier" },
+      typescriptreact = { "prettier" },
     },
   },
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,8 @@
 return {
   -- { "mason-org/mason.nvim", version = "2.0.0" },
   -- { "mason-org/mason-lspconfig.nvim", version = "2.0.0" },
+  -- opts = function(_, opts)
+  --   opts.ensure_installed = opts.ensure_installed or {}
+  --   table.insert(opts.ensure_installed, "js-debug-adapter")
+  -- end,
 }
