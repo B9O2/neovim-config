4b991326e7cd82e6dfb8b62c90b31f232e5e0f8f
Felix <112383606+B9O2@users.noreply.github.com>
2025-10-13 16:24:47 +0800

    add go support

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,8 +1,15 @@
 return {
+  "mason-org/mason.nvim",
   -- { "mason-org/mason.nvim", version = "2.0.0" },
   -- { "mason-org/mason-lspconfig.nvim", version = "2.0.0" },
-  -- opts = function(_, opts)
-  --   opts.ensure_installed = opts.ensure_installed or {}
-  --   table.insert(opts.ensure_installed, "js-debug-adapter")
-  -- end,
+  opts = {
+    ensure_installed = {
+      "goimports",
+      "gofumpt",
+      "gomodifytags",
+      "delve",
+      "impl",
+      "js-debug-adapter",
+    },
+  },
 }
