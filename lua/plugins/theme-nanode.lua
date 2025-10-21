9357d559194a73080c1f6e9f93136b3d88acb56c
Felix <112383606+B9O2@users.noreply.github.com>
2025-10-21 18:49:49 +0800

    vscode dashboard

--- lua/plugins/theme-nanode.lua
+++ lua/plugins/theme-nanode.lua
@@ -1,1 +1,10 @@
-return {}
+return {
+  "KijitoraFinch/nanode.nvim",
+  priority = 1000,
+  -- config = function()
+  --   require("nanode").setup({
+  --     transparent = false,
+  --   })
+  --   vim.cmd.colorscheme("nanode")
+  -- end,
+}
