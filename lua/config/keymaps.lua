3ca4034d46f5da1ace71455228a58a44eaed2a2b
Felix <112383606+B9O2@users.noreply.github.com>
2025-11-04 11:54:29 +0800

    - d will send things to _ - option as alt for neovide

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "d", '"_d')
+vim.keymap.set("n", "D", '"_D')
+
+vim.keymap.set("x", "d", '"_d')
