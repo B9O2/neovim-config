5c7ec2c0fc6340af7de5c674e878726d7c63c20d
Felix <112383606+B9O2@users.noreply.github.com>
2025-11-12 19:06:29 +0800

    80 column

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 --
+vim.opt.colorcolumn = "80"
 vim.opt.number = false
 vim.opt.relativenumber = false
 -- Neovide settings
