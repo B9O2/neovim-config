3c5a98bbe9cb0ba1d017757a7f560d300d15abf6
Felix <112383606+B9O2@users.noreply.github.com>
2025-12-01 19:14:18 +0800

    set keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,4 +4,8 @@
 
 vim.keymap.set("n", "d", '"_d')
 vim.keymap.set("n", "D", '"_D')
-vim.keymap.set("x", "d", '"_d')
+vim.keymap.set("v", "d", '"_d')
+
+vim.keymap.set("n", "c", '"_c')
+vim.keymap.set("n", "C", '"_C')
+vim.keymap.set("v", "c", '"_c')
