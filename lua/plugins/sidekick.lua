161cd5cb2938e8c47a726d3402d4faf950845a14
Felix <112383606+B9O2@users.noreply.github.com>
2026-01-30 18:23:04 +0800

    remove copilot

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -4,7 +4,25 @@ return {
   cmd = "Sidekick",
   ---@class sidekick.Config
   opts = {
+    copilot = {
+      status = {
+        enabled = false,
+      },
+    },
+    nes = {
+      enabled = false,
+    },
+    mux = {
+      enabled = false,
+      create = "window",
+    },
     cli = {
+      win = {
+        layout = "right",
+        split = {
+          height = 15,
+        },
+      },
       prompts = {
         haunt_all = function()
           return haunt_sk.get_locations()
@@ -13,6 +31,15 @@ return {
           return haunt_sk.get_locations({ current_buffer = true })
         end,
       },
+    },
+  },
+  keys = {
+    {
+      "<leader>ag",
+      function()
+        require("sidekick.cli").toggle({ name = "gemini", focus = true })
+      end,
+      desc = "Sidekick Toggle Gemini",
     },
   },
 }
