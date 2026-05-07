747d97a7d5ad3659e646b3d61a6c67741b9cffa3
Felix <112383606+B9O2@users.noreply.github.com>
2026-05-07 18:48:59 +0800

    better colorscheme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -3,7 +3,14 @@ local theme_name = "rose_pine"
 local variant_name = "moon"
 
 local themes = {
-  rose_pine = { "rose-pine/neovim", name = "rose-pine", variant_format = "rose-pine-%s" },
+  rose_pine = {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      styles = { transparency = true },
+    },
+    variant_format = "rose-pine-%s",
+  },
 }
 
 local theme = themes[theme_name]
@@ -11,18 +18,26 @@ if theme then
   theme.lazy = false
   theme.priority = 1000
 
-  local cmd = nil
-  if theme.variant_format then
-    cmd = "colorscheme " .. string.format(theme.variant_format, variant_name)
-  else
-    cmd = "colorscheme " .. theme.name
-  end
+  -- local cmd = nil
+  -- if theme.variant_format then
+  --   cmd = "colorscheme " .. string.format(theme.variant_format, variant_name)
+  -- else
+  --   cmd = "colorscheme " .. theme.name
+  -- end
+  --
+  -- theme.config = function()
+  --   vim.cmd(cmd)
+  -- end
 
-  theme.config = function()
-    vim.cmd(cmd)
-  end
-
-  return theme
+  return {
+    theme,
+    {
+      "LazyVim/LazyVim",
+      opts = {
+        colorscheme = string.format(theme.variant_format, variant_name),
+      },
+    },
+  }
 end
 
 return {}
